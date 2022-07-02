2237cac034032eb1bd8190da3cb15a9ee8fa9817
Veronika <ricka16.16@gmail.com>
2022-07-02 19:06:09 +0400

    blog page
    страница блога

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -3,23 +3,39 @@
 document.addEventListener('DOMContentLoaded', () => {
     let lastScrollPostion = 0;
     let isVisible = true;
+<<<<<<< Updated upstream
+=======
+    
+>>>>>>> Stashed changes
 
     function headerVisibleControl(isVisibleNew) {
-        if (isVisibleNew == isVisible)
+        if (isVisibleNew == isVisible){
             return;
+        }
+            
+        
         
         document.querySelector('.header').classList.toggle('header__hidden');
         isVisible = isVisibleNew;
     }
 
     window.addEventListener('scroll', (e) => {
+        
         var st = window.pageYOffset;
+<<<<<<< Updated upstream
         var scroll = window.scrollTop;
         if (scroll >= 50){
             document.querySelector('.header').classList.toggle('scrolled')
         }
+=======
+        var page = document.querySelector('body');
+        console.log(window.pageYOffset);
+        
+        
+>>>>>>> Stashed changes
 
         if (st > lastScrollPostion) {
+            
             // down scroll
             headerVisibleControl(false);
         } else {
